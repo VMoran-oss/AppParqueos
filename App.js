03720f2674945914697f6f63b4b0abf246dc8044
Veronica Moran <veronicamoran25@outlook.com>
2025-10-20 19:42:28 -0600

    correcion de errores

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react'; 
-import MallSelectionScreen from './screens/MallSelectionScreen';
 // importar tabs
 import AppTabs from './navigation/AppTabs';
 
 // importar screens que no se usan en los tabs
 import LoginScreen from './screens/LoginScreen';
+{/*import MallSelectionScreen from './screens/MallSelectionScreen';*/}
 import ParkingMapScreen from './screens/ParkingMapScreen';
 
 // importar react navigation
@@ -22,12 +22,12 @@ export default function App() {
         <Stack.Screen name="Home" component={AppTabs} options={{ headerShown: false }} />
         {/* <Stack.Screen name="Login" component={LoginScreen} options={{ title: "Ingresar" }} />
         <Stack.Screen name="Parking Map" component={ParkingMapScreen} options={{ title: "Ver Parqueos" }} /> */}
-
-        <Stack.Screen 
+        {/*<Stack.Screen name="Register" component={RegisterScreen} options={{ title: "Registrarse" }} />*/}
+       {/*} <Stack.Screen 
           name="MallSelection" 
           component={MallSelectionScreen} 
-          options={{ title: "Seleccionar Centro Comercial" }} 
-        />
+          options={{ title: "Seleccionar Centro Comercial" }}
+        />*/}
       </Stack.Navigator>
     </NavigationContainer>
   );
